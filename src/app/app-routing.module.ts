d9e427b0ef5a6294d04a00592d847c535f0a35e9
Daniel Duarte <danielduarte2004@gmail.com>
2018-07-18 18:07:04 -0400

    Created basic component and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { DataStorageComponent } from './components/Products/data-storage/data-storage.component';
+import { ServicesComponent } from './components/Products/services/services.component';
+import { ServersComponent } from './components/Products/servers/servers.component';
+import { AllProductsComponent } from './components/Products/all-products/all-products.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    children: [
+      { path: '', redirectTo: 'all', pathMatch: 'full' },
+      { path: 'all', component: AllProductsComponent },
+      { path: 'data-storage', component: DataStorageComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'servers', component: ServersComponent }
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AllProductsComponent } from './components/Products/all-products/all-products.component';
+import { ServersComponent } from './components/Products/servers/servers.component';
+import { ServicesComponent } from './components/Products/services/services.component';
+import { DataStorageComponent } from './components/Products/data-storage/data-storage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    AllProductsComponent,
+    ServersComponent,
+    ServicesComponent,
+    DataStorageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
