fcdec224fcc93b9c73ffb401671dee6e5d26fb41
Daniel Duarte <danielduarte2004@gmail.com>
2018-08-08 12:14:54 -0400

    Added content to products/ProductsOverviewComponent

--- src/app/components/products/products-overview/products-overview.component.ts
+++ src/app/components/products/products-overview/products-overview.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-overview.component.scss']
 })
 export class ProductsOverviewComponent implements OnInit {
+  public titles = ['DevHosts\' list of products', 'Our list of servers', 'Our list of services', 'Our list of storage centers'];
 
   constructor() { }
 
